eed3c94f9aa4a6847a8bc52c7a3298ae0890a77b
hectorgordillogarcia <hectorgordillogarcia@gmail.com>
2025-04-07 01:47:08 +0200

    added new testvideo2 code

--- temp/TestMovement&Gripper.py
+++ temp/TestMovement&Gripper.py
@@ -7,25 +7,56 @@ import time
 rtde_c = rtde_control.RTDEControlInterface("169.254.12.28")
 rtde_r = rtde_receive.RTDEReceiveInterface("169.254.12.28")
 rtde_i_o = rtde_io.RTDEIOInterface("169.254.12.28")
+bOk=False; 
+
+target_situate=[0.064,0.472,0.086,0.477,-3.25,-0.012]
+target_Aproximate=[0.107,0.387,0.105,0.025,3.13,-0.068]
+
+print("target_Aproximate:", target_Aproximate)  # Z aumentado en 0.15
 
 
+# if rtde_c.getInverseKinematicsHasSolution(target_begin):
+#     rtde_c.moveJ_IK(target_begin, 0.2, 0.2)
+#     time.sleep(1)
 
-target_begin=[-0.0177,0.495,0.270,3.16,-1.77,-0.031]
-rtde_c.moveJ_IK(target_begin, 0.2, 0.2)
+if rtde_c.getInverseKinematicsHasSolution(target_Aproximate):
+        rtde_c.moveJ_IK(target_Aproximate, 0.2, 0.2)
+        time.sleep(0.5)
 
-#Ensure that digital output is set to 0
-rtde_i_o.setToolDigitalOut(1,False)
-time.sleep(0.1)
+target_Pick=rtde_r.getActualTCPPose()
+target_Pick[2]-=0.5
 
-#Close
-rtde_i_o.setToolDigitalOut(1,True)
+rtde_c.moveL(target_Pick,0.2,0.2)
 time.sleep(0.5)
 
+# target_Pick=rtde_r.getActualTCPPose()
+# target_Pick[2] += 0.10
+
+#rtde_c.moveJ_IK(target_Aproximate, 0.2, 0.2)
+#time.sleep(0.5)
+
+#rtde_c.moveJ_IK(target_Pick, 0.2, 0.2)
+
+#rtde_c.moveL(target_Pick,0.2,0.2)
+#time.sleep(0.5)
+
+#Ensure that digital output is set to 0
+#rtde_i_o.setToolDigitalOut(0,False)
+#time.sleep(0.1)
+
+#Close
+rtde_i_o.setToolDigitalOut(0,False)
+rtde_i_o.setToolDigitalOut(1,True)
+
+time.sleep(1)
+
 target_end=[-0.0177,0.495,0.5,3.16,-1.77,-0.031]
-rtde_c.moveJ_IK(target_end, 0.2, 0.2)
+# rtde_c.moveJ_IK(target_end, 0.2, 0.2)
 
 #Open
 rtde_i_o.setToolDigitalOut(1,False)
-time.sleep(0.5)
+rtde_i_o.setToolDigitalOut(0,True)
+
+time.sleep(1)
 
 #Wait 1 second to ensure that the gripper is opened
