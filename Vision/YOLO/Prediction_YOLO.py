c220be01c75b90cbe48302ede8f44c085aef4739
hectorgordillogarcia <hectorgordillogarcia@gmail.com>
2025-04-10 20:00:05 +0200

    Change Yolo Prediction Added model

--- Vision/YOLO/Prediction_YOLO.py
+++ Vision/YOLO/Prediction_YOLO.py
@@ -2,46 +2,35 @@ import cv2
 import os
 from ultralytics import YOLO
 
+def getImage():
+    #Aqui saco la foto con la camara
+    #return imagen
+    pass
+
 # Función para realizar la detección y guardar la imagen
-def realizar_deteccion(img_name, model_path):
-    # Cargar la imagen
+def getPrediction(img_name, model_path):
+    conf_threshold = 0.8
     img = cv2.imread(img_name)
-
-    # Cargar el modelo
     model = YOLO(model_path)
-
-    # Realizar la predicción
-    results = model.predict(img)
-
-    # Los resultados se encuentran en 'results' y contienen las predicciones
-    pred = results[0]  # Obtener el primer resultado de la predicción
-
-    pred_img = pred.plot()
-
-    # Guardar la imagen con la detección
-    cv2.imwrite(f"{img_name}_deteccion.png", pred_img)
-
-    return img, pred  # Retornar la imagen original y la predicción
+    results = model.predict(source=img, conf=conf_threshold)
+    return results
 
 # # Función para calcular el centroide de las cajas de predicción
-def calcular_centroides(pred):
+def getCentroid(results):
+    pred=results[0]
     centroids = []
-    
-    # Los resultados de la predicción tienen un atributo 'boxes' que es un ndarray
-    # Acceder a las coordenadas de las cajas
     boxes = pred.boxes.xyxy  # Coordenadas de las cajas (x_min, y_min, x_max, y_max)
 
-    # Iterar sobre las cajas y calcular el centroide
     for box in boxes:
         x_min, y_min, x_max, y_max = box
-        # Calcular las coordenadas del centroide (promedio de las coordenadas x y y)
         centroide_x = (x_min + x_max) / 2
         centroide_y = (y_min + y_max) / 2
         centroids.append((centroide_x, centroide_y))
     return centroids
 
+def getCanCentroid(Selection):
 
-if __name__ == '__main__':
+def main():
     # Define la ruta base relativa a este script
     base_dir = os.path.dirname(__file__)  # Directorio del script
 
@@ -50,7 +39,12 @@ if __name__ == '__main__':
     model_path = os.path.join(base_dir, "best_model.pt")
 
     # Llamar a la función para realizar la detección y obtener la predicción
-    img, pred = realizar_deteccion(img_name, model_path)
+    img, pred = getPrediction(img_name, model_path)
 
     # Llamar a la función para calcular los centroides
-    centroides = calcular_centroides(pred)
+    centroides = getCentroid(pred)
+
+
+
+if __name__ == '__main__':
+    main()
